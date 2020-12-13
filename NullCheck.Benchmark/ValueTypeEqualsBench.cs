b84b8a792f909fbdc83d1b7dabd6f8930e4fa104
MeilCli <ics.sora.meil@gmail.com>
2020-12-13 18:26:58 +0900

    add ValueType.Equals runtime compare case

---
+++ NullCheck.Benchmark/ValueTypeEqualsBench.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace NullCheck.Benchmark
+{
+    // dotnet run -c Release -f net48
+    [SimpleJob(RuntimeMoniker.Net48)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp50)]
+    [SimpleJob(RuntimeMoniker.Mono)]
+    [MeanColumn, MinColumn, MaxColumn]
+    [MemoryDiagnoser]
+    public class ValueTypeEqualsBench
+    {
+        private object expect = 1;
+
+        [Params(null, 1)]
+        public object? Value { get; set; }
+
+        [Benchmark]
+        public bool ValueTypeEquals()
+        {
+            return object.Equals(Value, expect);
+        }
+    }
+}
---
+++ NullCheck.Benchmark/ValueTypeLongStructEqualsBench.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using System.Collections.Generic;
+
+namespace NullCheck.Benchmark
+{
+    // dotnet run -c Release -f net48
+    [SimpleJob(RuntimeMoniker.Net48)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp50)]
+    [SimpleJob(RuntimeMoniker.Mono)]
+    [MeanColumn, MinColumn, MaxColumn]
+    [MemoryDiagnoser]
+    public class ValueTypeLongStructEqualsBench
+    {
+        public struct BigStruct
+        {
+            public long Value1;
+            public long Value2;
+            public long Value3;
+            public long Value4;
+            public long Value5;
+            public long Value6;
+            public long Value7;
+            public long Value8;
+        }
+
+        private object expect = new BigStruct();
+
+        public IEnumerable<object> Source()
+        {
+            yield return new BigStruct();
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Source))]
+        public bool ValueTypeEquals(object value)
+        {
+            return object.Equals(value, expect);
+        }
+    }
+}
